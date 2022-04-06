94d9c42362fb0fe0735f6d7cfe14813ebaa385c4
rembrants <rembrants@yahoo.com>
2022-04-06 11:50:04 +0700

    Add files via upload

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -41,7 +41,7 @@ myConfig = def { modMask = mod1Mask
   , workspaces = myWorkspaces
   , terminal = "xterm" 
   , handleEventHook = handleEventHook def <+> Hacks.windowedFullscreenFixEventHook
-  , manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def
+  , manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def <+> (className =? "XClock" --> doIgnore)
   , startupHook = do
      spawnOn "1:www" "chromium --start-fullscreen"
   ,logHook = updatePointer (0.5, 0.5) (0, 0) -- exact centre of window
@@ -52,10 +52,12 @@ myConfig = def { modMask = mod1Mask
   [
   ("M-p", spawn "rofi -show run -theme dmenu -display-run '$'")
   , ("M-<Return>", swapPromote' False)
-  , ("M-c", spawn "chromium --incognito")
   , ("M-C-l", spawn "pyxtrlock")
   , ("M-z", sendMessage MirrorShrink)
   , ("M-a", sendMessage MirrorExpand)
+  , ("M-o", sendMessage Toggle)
+  , ("M-u", sendMessage MagnifyMore)
+  , ("M-i", sendMessage MagnifyLess)
   ]
 
 
@@ -67,7 +69,8 @@ myLayout = onWorkspace "1:www" layoutMirror $
      -- default tiling algorithm partitions the screen into two panes
      tiled  		 = renamed [Replace "Tall"] $ ResizableTall 1 (3/100) (1/2) []
      layoutMirror 	 = renamed [Replace "Mirror"] $ smartBorders $ Mirror $ ResizableTall 1 (3/100) (85/100) []
-     layoutMagnifier	 = renamed [Replace "Magnifier"] $ lessBorders Screen $ magnifiercz' 1.4 (Tall 1 (3/100)(60/100))
+     layoutMagnifier	 = lessBorders Screen $ maximizeVertical $ (Tall 1 (3/100)(60/100))
+     --layoutMagnifier	 = renamed [Replace "Magnifier"] $ lessBorders Screen $ magnifiercz' 1.4 (Tall 1 (3/100)(60/100))
      layoutSpiral 	 = spiral (6/7)
      layoutThreecol 	 = ThreeCol 1 (3/100) (1/2)
      --layoutMulticol 	 = multiCol [1] 1 0.01 (-0.5)
