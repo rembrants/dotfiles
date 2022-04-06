59d509a7b9e7d25f200ab97a94b5e2434536ab79
rembrants <rembrants@yahoo.com>
2022-04-06 10:16:33 +0700

    Add files via upload

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,7 +13,11 @@ import XMonad.Layout.Grid
 import XMonad.Layout.Renamed
 import XMonad.Layout.Magnifier
 import XMonad.Layout.PerWorkspace
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.BorderResize
 import XMonad.Actions.SpawnOn
+import XMonad.Actions.UpdatePointer
+import XMonad.Actions.SwapPromote
 
 -- The main function.
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
@@ -33,37 +37,40 @@ myConfig = def { modMask = mod1Mask
   , layoutHook = myLayout
   , normalBorderColor = "#00ffff"
   , focusedBorderColor = "#ff00ff"
-  , focusFollowsMouse = False
+  , focusFollowsMouse = True
   , workspaces = myWorkspaces
   , terminal = "xterm" 
   , handleEventHook = handleEventHook def <+> Hacks.windowedFullscreenFixEventHook
   , manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def
   , startupHook = do
      spawnOn "1:www" "chromium --start-fullscreen"
+  ,logHook = updatePointer (0.5, 0.5) (0, 0) -- exact centre of window
+   >> masterHistoryHook
   }
 
   `additionalKeysP` 
   [
   ("M-p", spawn "rofi -show run -theme dmenu -display-run '$'")
+  , ("M-<Return>", swapPromote' False)
   , ("M-c", spawn "chromium --incognito")
-  , ("M-x", spawn "pyxtrlock")
+  , ("M-C-l", spawn "pyxtrlock")
   , ("M-z", sendMessage MirrorShrink)
   , ("M-a", sendMessage MirrorExpand)
   ]
 
 
 myLayout = onWorkspace "1:www" layoutMirror $
-           onWorkspace "2:net" layoutMulticol $
+           onWorkspace "2:net" Grid $
            onWorkspace "3:dev" layoutMagnifier $
-           noBorders Full ||| tiled ||| layoutThreecol ||| layoutSpiral ||| Grid ||| Mirror tiled
+           noBorders Full ||| tiled ||| layoutThreecol ||| layoutSpiral ||| borderResize emptyBSP ||| Mirror tiled
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled  		 = renamed [Replace "Tall"] $ ResizableTall 1 (3/100) (1/2) []
-     layoutMirror 	 = renamed [Replace "Mirror"] $ Mirror $ ResizableTall 1 (3/100) (85/100) []
-     layoutMagnifier	 = renamed [Replace "Magnifier"] $ magnifiercz' 1.4 (Tall 1 (3/100)(60/100))
+     layoutMirror 	 = renamed [Replace "Mirror"] $ smartBorders $ Mirror $ ResizableTall 1 (3/100) (85/100) []
+     layoutMagnifier	 = renamed [Replace "Magnifier"] $ lessBorders Screen $ magnifiercz' 1.4 (Tall 1 (3/100)(60/100))
      layoutSpiral 	 = spiral (6/7)
      layoutThreecol 	 = ThreeCol 1 (3/100) (1/2)
-     layoutMulticol 	 = multiCol [1] 1 0.01 (-0.5)
+     --layoutMulticol 	 = multiCol [1] 1 0.01 (-0.5)
 
      --tiled   = Tall nmaster delta ratio
      
