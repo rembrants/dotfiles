269958ccb012d23c165e9bc21c35dbca12f5ae48
rembrants <rembrants@yahoo.com>
2024-11-19 14:27:00 +0700

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,7 @@
 -- Imports.
 import XMonad
+import XMonad.ManageHook
+import XMonad.Hooks.ManageHelpers
 import qualified XMonad.Util.Hacks as Hacks
 import XMonad.Layout.ResizableTile
 import XMonad.Hooks.DynamicLog
@@ -23,7 +25,7 @@ import XMonad.Actions.SwapPromote
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 myBar = "xmobar"
 
-myWorkspaces = ["1:www", "2:net", "3:dev", "4:log"]
+myWorkspaces = ["1", "2", "3", "4"]
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
 myPP = xmobarPP { ppCurrent = xmobarColor "#aaaaaa" "" . wrap "[" "]" }
@@ -35,15 +37,15 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 -- Main configuration, override the defaults to your liking.
 myConfig = def { modMask = mod1Mask
   , layoutHook = myLayout
-  , normalBorderColor = "#00ffff"
-  , focusedBorderColor = "#ff00ff"
+--  , normalBorderColor = "#00ffff"
+  , normalBorderColor = "#000000"
+--  , focusedBorderColor = "#ff00ff"
   , focusFollowsMouse = True
   , workspaces = myWorkspaces
   , terminal = "xterm" 
   , handleEventHook = handleEventHook def <+> Hacks.windowedFullscreenFixEventHook
-  , manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def <+> (className =? "XClock" --> doIgnore)
-  , startupHook = do
-     spawnOn "1:www" "chromium --start-fullscreen"
+  , manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def <+> (className =? "wlclock" --> doIgnore)
+  --, manageHook = insertPosition End Newer <+> manageSpawn <+> manageHook def <+> (className =? "org.poul.wlclock" --> doIgnore)
   ,logHook = updatePointer (0.5, 0.5) (0, 0) -- exact centre of window
    >> masterHistoryHook
   }
@@ -52,27 +54,23 @@ myConfig = def { modMask = mod1Mask
   [
   ("M-p", spawn "rofi -show run -theme dmenu -display-run '$'")
   , ("M-<Return>", swapPromote' False)
-  , ("M-C-l", spawn "pyxtrlock")
   , ("M-z", sendMessage MirrorShrink)
   , ("M-a", sendMessage MirrorExpand)
-  , ("M-o", sendMessage Toggle)
-  , ("M-u", sendMessage MagnifyMore)
-  , ("M-i", sendMessage MagnifyLess)
   ]
 
 
-myLayout = onWorkspace "1:www" layoutMirror $
-           onWorkspace "2:net" Grid $
-           onWorkspace "3:dev" layoutMagnifier $
-           noBorders Full ||| tiled ||| layoutThreecol ||| layoutSpiral ||| borderResize emptyBSP ||| Mirror tiled
+--myLayout = onWorkspace "1" tiled$
+myLayout = 
+          tiled ||| Mirror tiled  |||  noBorders Full
+          -- noBorders Full ||| tiled ||| layoutThreecol ||| layoutSpiral ||| borderResize emptyBSP ||| Grid ||| Mirror tiled
   where
      -- default tiling algorithm partitions the screen into two panes
-     tiled  		 = renamed [Replace "Tall"] $ ResizableTall 1 (3/100) (1/2) []
-     layoutMirror 	 = renamed [Replace "Mirror"] $ smartBorders $ Mirror $ ResizableTall 1 (3/100) (85/100) []
-     layoutMagnifier	 = lessBorders Screen $ maximizeVertical $ (Tall 1 (3/100)(60/100))
+     tiled     = renamed [Replace "Tall"] $ ResizableTall 1 (3/100) (1/2) []
+     layoutMirror    = renamed [Replace "Mirror"] $ smartBorders $ Mirror $ ResizableTall 1 (3/100) (85/100) []
+     --layoutMagnifier	 = lessBorders Screen $ maximizeVertical $ (Tall 1 (3/100)(60/100))
      --layoutMagnifier	 = renamed [Replace "Magnifier"] $ lessBorders Screen $ magnifiercz' 1.4 (Tall 1 (3/100)(60/100))
-     layoutSpiral 	 = spiral (6/7)
-     layoutThreecol 	 = ThreeCol 1 (3/100) (1/2)
+     --layoutSpiral 	 = spiral (6/7)
+     --layoutThreecol 	 = ThreeCol 1 (3/100) (1/2)
      --layoutMulticol 	 = multiCol [1] 1 0.01 (-0.5)
 
      --tiled   = Tall nmaster delta ratio
@@ -86,4 +84,8 @@ myLayout = onWorkspace "1:www" layoutMirror $
      -- Default proportion of screen occupied by master pane
      --ratio   = 1/2
 
-    
+ --myManageHook = composeAll
+ --   [ className =? "Wayland Clock" --> doF W.swapDown
+ --   , className =? "Wayland Clock" --> doIgnore
+ --   , isFullscreen --> doFullFloat
+ --   ] 
